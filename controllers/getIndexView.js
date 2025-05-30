3fab49f5a8cebbb2635b6a4929dc15fff01c884e
samuel <samawyck@gmail.com>
2025-05-30 14:31:08 -0500

    Finish index controller

--- controllers/getIndexView.js
+++ controllers/getIndexView.js
@@ -1,7 +1,15 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db.js");
+
 
 const getIndexView = asyncHandler(async function(req, res) {
-    return res.send("Index");
+    const messages = await db.getAllMessages();
+
+    if (!messages) {
+        throw new Error("Server error");
+    }
+
+    return res.render("index", {messages: messages});
 });
 
 
