1965b8bb28feafde1579d0aa7c07e4e6afdbff1d
samuel <samawyck@gmail.com>
2025-05-30 15:18:33 -0500

    Add /new route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,13 @@
 const {Router} = require("express");
 const getIndexView = require("../controllers/getIndexView.js");
+const getNewMessageForm = require("../controllers/getNewMessageForm.js");
 
 
 const indexRouter = Router();
 
 
 indexRouter.get("/", getIndexView);
+indexRouter.get("/new", getNewMessageForm);
 
 
 module.exports = indexRouter;
