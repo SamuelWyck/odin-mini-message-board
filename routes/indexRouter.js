c6b9d48f10887b52997cc942d18a4c1295060f48
samuel <samawyck@gmail.com>
2025-05-30 16:04:04 -0500

    Add /new post route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,7 @@
 const {Router} = require("express");
 const getIndexView = require("../controllers/getIndexView.js");
 const getNewMessageForm = require("../controllers/getNewMessageForm.js");
+const addNewMessage = require("../controllers/addNewMessage.js");
 
 
 const indexRouter = Router();
@@ -8,6 +9,7 @@ const indexRouter = Router();
 
 indexRouter.get("/", getIndexView);
 indexRouter.get("/new", getNewMessageForm);
+indexRouter.post("/new", addNewMessage);
 
 
 module.exports = indexRouter;
