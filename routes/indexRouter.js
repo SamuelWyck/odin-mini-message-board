c06014bcd953baffc916cca573be0846e23a4667
samuel <samawyck@gmail.com>
2025-05-30 16:47:38 -0500

    Add message/:id route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,7 @@ const {Router} = require("express");
 const getIndexView = require("../controllers/getIndexView.js");
 const getNewMessageForm = require("../controllers/getNewMessageForm.js");
 const addNewMessage = require("../controllers/addNewMessage.js");
+const getMessageDetails = require("../controllers/getMessageDetailsView.js");
 
 
 const indexRouter = Router();
@@ -10,6 +11,7 @@ const indexRouter = Router();
 indexRouter.get("/", getIndexView);
 indexRouter.get("/new", getNewMessageForm);
 indexRouter.post("/new", addNewMessage);
+indexRouter.get("/message/:id", getMessageDetails);
 
 
 module.exports = indexRouter;
