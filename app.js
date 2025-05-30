f0792c857c6063cfd6b450a9f4cb779034ed65ec
samuel <samawyck@gmail.com>
2025-05-30 16:48:11 -0500

    Add error catch to app

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.use(express.urlencoded({extended: true}));
 app.use("/", indexRouter);
 
 
+app.use(function(error, req, res, next) {
+    res.redirect("/");
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 
