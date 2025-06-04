091fe20e19fb623261d1b38fb2a1b15192dd2346
samuel <samawyck@gmail.com>
2025-06-04 14:55:44 -0500

    Change pool connection

--- db/pool.js
+++ db/pool.js
@@ -3,5 +3,5 @@ require("dotenv").config()
 
 
 module.exports = new Pool({
-    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB}`
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB}?sslmode=require`
 });
